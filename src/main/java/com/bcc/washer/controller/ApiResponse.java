b3ca20b7d2b40782babc9a8e4a4e3666bd0d0a24
minerkg <129666850+minerkg@users.noreply.github.com>
2025-05-11 18:19:03 +0300

    main structure added

---
+++ src/main/java/com/bcc/washer/controller/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.bcc.washer.controller;
+
+public class ApiResponse<T> {
+
+    private String header;
+    private T body;
+
+    public ApiResponse(String header, T body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public T getBody() {
+        return body;
+    }
+
+    public void setBody(T body) {
+        this.body = body;
+    }
+}
---
+++ src/main/java/com/bcc/washer/controller/ResourceController.java
@@ -0,0 +1,18 @@
+package com.bcc.washer.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceController {
+
+    @GetMapping("/test")
+    public ResponseEntity<ApiResponse<String>> getTest() {
+        return ResponseEntity.ok(new ApiResponse<>("test", "test" ));
+    }
+
+}
