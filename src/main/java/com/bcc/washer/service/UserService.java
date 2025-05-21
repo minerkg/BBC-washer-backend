1a2c89c54939ff9733950ebdb624f12562e0644b
ZsoldicsBalazs <119108270+ZsoldicsBalazs@users.noreply.github.com>
2025-05-21 22:41:44 +0300

    basic auth implemented with user entity with roles

---
+++ src/main/java/com/bcc/washer/service/UserService.java
@@ -0,0 +1,18 @@
+package com.bcc.washer.service;
+
+import com.bcc.washer.domain.User;
+import com.bcc.washer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
