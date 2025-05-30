dc6e3a58aa8b62ac66a73353038047fadcb1159d
Csiszer <csiszerors@gmail.com>
2025-05-30 11:53:58 +0300

    service find all methods added

--- src/main/java/com/bcc/washer/service/BookableUnitService.java
+++ src/main/java/com/bcc/washer/service/BookableUnitService.java
@@ -1,14 +1,13 @@
 package com.bcc.washer.service;
 
 
-import com.bcc.washer.controller.ApiResponse;
 import com.bcc.washer.domain.BookableUnit;
 import com.bcc.washer.repository.BookableUnitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class BookableUnitService {
@@ -18,9 +17,19 @@ public class BookableUnitService {
 
 
     public Set<BookableUnit> getAllAvailableBookableUnits() {
+        return bookableUnitRepository.findAll()
+                .stream()
+                .filter(BookableUnit::isAvailable)
+                .collect(Collectors.toSet());
 
     }
 
     public Set<BookableUnit> getAllABookableUnitsHistoryByUser(Long userId) {
+        //// not working yet
+
+        return bookableUnitRepository.findAll()
+                .stream()
+                .filter(BookableUnit::isAvailable)
+                .collect(Collectors.toSet());
     }
 }
