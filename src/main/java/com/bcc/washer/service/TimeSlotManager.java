1cafab9815680f762c40061e9d8bee588a7d4156
minerkg <129666850+minerkg@users.noreply.github.com>
2025-06-08 13:46:17 +0300

    time slot management creation is done with minor improvements

--- src/main/java/com/bcc/washer/service/TimeSlotManager.java
+++ src/main/java/com/bcc/washer/service/TimeSlotManager.java
@@ -30,31 +30,22 @@ public class TimeSlotManager {
     }
 
     @Transactional
-    public List<TimeSlot> createTimeSlots(LocalDate from, LocalDate to, List<BookableUnit> bookableUnits) {
-        LocalDate newTimeSlotStartDay = from.plusDays(1);
+    public List<TimeSlot> createTimeSlots(LocalDate timeSlotStartDayFrom, LocalDate to, List<BookableUnit> bookableUnits) {
 
-        TimeSlot latestExistingTimeSlot = timeSlotRepository.findAll().stream()
-                .max(Comparator.comparing(ts -> ts.getTimeInterval().getEndTime()))
-                .orElse(TimeSlot
-                        .builder()
-                        .timeInterval(TimeInterval
-                                .builder()
-                                .startTime(OPENINGHOURS.CLOSE.getTime().minusHours(1))
-                                .endTime(OPENINGHOURS.CLOSE.getTime())
-                                .date(from)
-                                .build())
-                        .build());
+        //if there is no available timeslot than it generates one with startsDay := from -1
+        TimeSlot latestExistingTimeSlot = getLatestExistingTimeSlot(timeSlotStartDayFrom);
+
         if (latestExistingTimeSlot.getTimeInterval().getDate().isAfter(to)) {
             throw new RuntimeException("TimeSlots are already available");
         }
-        if (latestExistingTimeSlot.getTimeInterval().getDate().isAfter(from)) {
-            newTimeSlotStartDay = latestExistingTimeSlot.getTimeInterval().getDate().plusDays(1);
+        if (latestExistingTimeSlot.getTimeInterval().getDate().isAfter(timeSlotStartDayFrom)) {
+            timeSlotStartDayFrom = latestExistingTimeSlot.getTimeInterval().getDate().plusDays(1);
         }
 
         //generating TimeSlots
         List<TimeSlot> newlyAvailableTimeSlots = new ArrayList<>();
 
-        for (LocalDate date = newTimeSlotStartDay; !date.isAfter(to); date = date.plusDays(1)) {
+        for (LocalDate date = timeSlotStartDayFrom; !date.isAfter(to); date = date.plusDays(1)) {
             for (LocalTime timeIntervalStart = OPENINGHOURS.OPEN.getTime();
                  timeIntervalStart.isBefore(OPENINGHOURS.CLOSE.getTime());
                  timeIntervalStart = timeIntervalStart.plusHours(1)) {
@@ -68,10 +59,27 @@ public class TimeSlotManager {
                                 .build())
                         .bookableUnit(bookableUnits)
                         .build();
-                newlyAvailableTimeSlots.add(timeSlotRepository.save(newTimeSlot));
+                newlyAvailableTimeSlots.add(newTimeSlot);
             }
         }
+        timeSlotRepository.saveAll(newlyAvailableTimeSlots);
+
         return newlyAvailableTimeSlots;
+    }
+
+    private TimeSlot getLatestExistingTimeSlot(LocalDate from) {
+        return timeSlotRepository.findAll().stream()
+                .max(Comparator.comparing(ts -> ts.getTimeInterval().getEndTime()))
+                .orElse(TimeSlot
+                        .builder()
+                        .timeInterval(TimeInterval
+                                .builder()
+                                .startTime(OPENINGHOURS.CLOSE.getTime().minusHours(1))
+                                .endTime(OPENINGHOURS.CLOSE.getTime())
+                                .date(from.minusDays(1))
+                                .build())
+                        .build());
+
     }
 
 
