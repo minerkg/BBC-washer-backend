248e652983acbdb93423299625e1a991f14b5e53
Csiszer <csiszerors@gmail.com>
2025-06-20 16:42:23 +0300

    new bookable unit repo method added

--- src/main/java/com/bcc/washer/controller/BookableUnitController.java
+++ src/main/java/com/bcc/washer/controller/BookableUnitController.java
@@ -10,10 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -65,8 +62,8 @@ public class BookableUnitController {
         }
     }
 
-    @GetMapping("")
-    public ResponseEntity<ApiResponse<Set<BookableUnitDto>>> getAllAvailableBookableUnitsByDay(LocalDate localDate) {
+    @GetMapping("/by-date")
+    public ResponseEntity<ApiResponse<Set<BookableUnitDto>>> getAllAvailableBookableUnitsByDay(@RequestBody LocalDate localDate) {
         try {
             logger.info("---  getAllBookableUnitsByDay method accessed  ---");
             var bookableUnits = bookableUnitService.getAllAvailableBookableUnitsWithinOneWeek(localDate);
