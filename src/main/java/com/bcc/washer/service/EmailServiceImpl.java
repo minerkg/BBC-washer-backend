a840cd2b3ee33ffe6b321aefa41e4fb4f489171e
csiszer <129666850+minerkg@users.noreply.github.com>
2025-06-26 21:26:00 +0200

    Feature/notifications (#34)
    * cancellation notification added
    
    * cancellation notification added
    
    * prop config modified

--- src/main/java/com/bcc/washer/service/EmailServiceImpl.java
+++ src/main/java/com/bcc/washer/service/EmailServiceImpl.java
@@ -25,16 +25,19 @@ public class EmailServiceImpl implements NotificationServiceI {
     @Autowired
     private JavaMailSender emailSender;
 
+
+    private final String ADDRESS = "Cluj-Napoca, Teodor Mihali street 58-60";
+
     @Override
     public void notifyReservation(String destination, String subject, String text, BookableUnit bookableUnit) {
         Map<String, String> placeholders = new HashMap<>();
         placeholders.put("date", bookableUnit.getTimeSlot().getTimeInterval().getDate().toString());
         placeholders.put("time", bookableUnit.getTimeSlot().getTimeInterval().getStartTime().toString());
-        placeholders.put("location", "Cămin C1 - Etaj 2 Cluj Napoca");
+        placeholders.put("location", ADDRESS);
         placeholders.put("text", text);
 
         try {
-            String body = loadEmailTemplate(TemplateTYPE.REZERVATION_CREATED.getPath(), placeholders);
+            String body = loadEmailTemplate(TemplateTYPE.RESERVATION_CREATED.getPath(), placeholders);
             MimeMessage message = emailSender.createMimeMessage();
 
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
@@ -53,6 +56,37 @@ public class EmailServiceImpl implements NotificationServiceI {
 
 
     }
+
+    @Override
+    public void notifyCancellation(String destination, String subject, String text, BookableUnit bookableUnit) {
+        Map<String, String> placeholders = new HashMap<>();
+        placeholders.put("date", bookableUnit.getTimeSlot().getTimeInterval().getDate().toString());
+        placeholders.put("time", bookableUnit.getTimeSlot().getTimeInterval().getStartTime().toString());
+        placeholders.put("location", ADDRESS);
+        placeholders.put("text", text);
+
+        try {
+            String body = loadEmailTemplate(TemplateTYPE.RESERVATION_CANCELLED.getPath(), placeholders);
+            MimeMessage message = emailSender.createMimeMessage();
+
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            message.setFrom("bbcwasher@gmail.com");
+            helper.setTo(destination);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+
+            emailSender.send(message);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
 
     private String loadEmailTemplate(String path, Map<String, String> placeholders) throws IOException {
         String template = new String(
