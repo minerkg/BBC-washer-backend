d8585a4b244a8ab2ae13ec89c0bdb309164bec4c
csiszer <129666850+minerkg@users.noreply.github.com>
2025-06-27 21:24:05 +0200

    Corrections/all reservations get bug (#37)
    * getAll reservation by admin fixed
    
    * getAll reservation by admin fixed
    
    * getAllByUser cancelled reservation filtering deleted
    
    * getAllByUser cancelled reservation filtering deleted
    
    * prop file mod

--- src/main/java/com/bcc/washer/service/ReservationService.java
+++ src/main/java/com/bcc/washer/service/ReservationService.java
@@ -78,10 +78,8 @@ public class ReservationService {
         userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + userId));
 
-        // Filter for CONFIRMED bookings for the user, or all if you want history including cancelled
         return reservationRepository.findAll().stream()
                 .filter(reservation -> reservation.getUser().getId() == userId)
-                .filter(reservation -> reservation.getStatus() != ReservationStatus.CANCELLED) // Only show non-cancelled for user
                 .collect(Collectors.toSet());
     }
 
@@ -97,11 +95,18 @@ public class ReservationService {
 
         BookableUnit bookableUnit = reservation.getBookableUnit();
         if (bookableUnit != null) {
-            bookableUnit.setReservation(null);
-            bookableUnit.setAvailable(true); // Make the bookable unit available again
+            bookableUnit.setAvailable(false);
             bookableUnitRepository.save(bookableUnit);
+            //make a new bookable unit with the same timeslot id
+            var newlyAvailableBu = BookableUnit
+                    .builder()
+                    .washer(bookableUnit.getWasher())
+                    .isAvailable(true)
+                    .timeSlot(bookableUnit.getTimeSlot())
+                    .build();
+            bookableUnitRepository.save(newlyAvailableBu);
         }
-        reservation.setBookableUnit(null);
+
         reservation.setStatus(ReservationStatus.CANCELLED); // Set status to CANCELLED
         reservationRepository.save(reservation); // Save the updated reservation instead of deleting
     }
